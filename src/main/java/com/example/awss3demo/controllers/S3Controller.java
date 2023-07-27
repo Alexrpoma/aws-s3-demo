f015c4ba5e8ebefbe25de23977b85a9885311474
Alex Poma <alex.rpoma@outlook.com>
2023-07-27 18:23:55 -0400

    AWS-S3 Controller GetMapping

--- src/main/java/com/example/awss3demo/controllers/S3Controller.java
+++ src/main/java/com/example/awss3demo/controllers/S3Controller.java
@@ -2,10 +2,7 @@ package com.example.awss3demo.controllers;
 
 import com.example.awss3demo.services.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,6 +13,11 @@ import java.io.IOException;
 public class S3Controller {
 
   private final S3Service s3Service;
+
+  @GetMapping
+  public String welcome() {
+    return "Welcome to AWS!!";
+  }
 
   @PostMapping("/upload")
   public String uploadFile(@RequestParam("file")MultipartFile multipartFile) throws IOException {
