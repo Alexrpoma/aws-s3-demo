66cc0d96648b25cc8c7445e589f1a81e41af0a20
Alex Poma <alex.rpoma@outlook.com>
2023-07-27 18:06:37 -0400

    AWS-S3 Service

---
+++ src/main/java/com/example/awss3demo/services/S3Service.java
@@ -0,0 +1,31 @@
+package com.example.awss3demo.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+  private final S3Client s3Client;
+
+  public String uploadFile(MultipartFile multipartFile) throws IOException {
+    try {
+      String fileName = multipartFile.getOriginalFilename();
+      PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+          .bucket("aws-spring-alx-bucket0")
+          .key(fileName)
+          .build();
+      s3Client.putObject(putObjectRequest, RequestBody.fromBytes(multipartFile.getBytes()));
+      return "The file was uploaded successfully";
+    }catch (IOException ioe) {
+      throw new IOException(ioe.getMessage());
+    }
+  }
+}
