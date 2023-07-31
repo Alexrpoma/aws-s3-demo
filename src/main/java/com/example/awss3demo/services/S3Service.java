6a85ee716026999ccf2912b65d64b147cec037e7
Alex Poma <alex.rpoma@outlook.com>
2023-07-31 17:39:31 -0400

    AWS - Initial S3 file downloader method

--- src/main/java/com/example/awss3demo/services/S3Service.java
+++ src/main/java/com/example/awss3demo/services/S3Service.java
@@ -1,19 +1,27 @@
 package com.example.awss3demo.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class S3Service implements IS3Service{
 
   private final S3Client s3Client;
+
+  @Value("${download.file.path}")
+  private String downloadFilePath;
 
   @Override
   public String uploadFile(MultipartFile multipartFile) throws IOException {
@@ -32,6 +40,42 @@ public class S3Service implements IS3Service{
 
   @Override
   public String downloadFile(String fileName) throws IOException {
-    return null;
+    if (!isExistFile(fileName)) {
+      log.info("The file %s doesn't exist!".formatted(fileName));
+      return "The file %s doesn't exist!".formatted(fileName);
+    }
+    GetObjectRequest objectRequest = GetObjectRequest.builder()
+        .bucket("aws-spring-alx-bucket0")
+        .key(fileName)
+        .build();
+    ResponseInputStream<GetObjectResponse> responseInputStream = s3Client.getObject(objectRequest);
+
+    try(FileOutputStream fileOutputStream = new FileOutputStream(downloadFilePath)) {
+      byte[] read_buffer = new byte[1024];
+      int read_length = 0;
+      while((read_length = responseInputStream.read(read_buffer)) > 0) {
+        fileOutputStream.write(read_buffer, 0, read_length);
+      }
+    } catch (IOException ioException) {
+      log.error(ioException.getMessage());
+    }
+    return "File %s downloaded successfully!".formatted(fileName);
+  }
+
+  private boolean isExistFile(String objKey) {
+    try {
+      HeadObjectRequest objectRequest = HeadObjectRequest.builder()
+          .bucket("aws-spring-alx-bucket0")
+          .key(objKey)
+          .build();
+      s3Client.headObject(objectRequest);
+      return true;
+    } catch (S3Exception s3Exception) {
+      log.error(s3Exception.getMessage());
+      if(s3Exception.statusCode() == 404) {
+        return false;
+      }
+    }
+    return false;
   }
 }
